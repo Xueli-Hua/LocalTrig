40a15045eef0ce5a992eba05f3dfcf361f026cb4
Xueli Hua <150328932+Xueli-Hua@users.noreply.github.com>
2024-05-20 11:23:04 +0800

    Update rateZB.C

--- zdc/src/rateZB.C
+++ zdc/src/rateZB.C
@@ -170,7 +170,6 @@ int rate(char const* input) {
     TH2F hTrigvsSumMinus_Emu("hTrigvsSumMinus_Emu","hTrigvsSumMinus_Emu",16,0,16,5000, 0, 500);
     TH2F hTrigvsSumPlus_Emu("hTrigvsSumPlus_Emu","hTrigvsSumPlus_Emu",16,0,16,5000, 0, 500);
     TH1D hlumi;
-    TH1D hlumi[16];
     TH1D hlumi_itrig[16];
 	 
     Double_t zdcnum=0;
@@ -200,7 +199,6 @@ int rate(char const* input) {
 	    l1uGTEmudecisions[it]=m_algoDecisionInitial_Emu.At(SeedBit[seeds[it].c_str()]);
 	    l1uGTdecisions[it]=m_algoDecisionInitial_unpacker.At(SeedBit[seeds[it].c_str()]);
 	    if (l1uGTEmudecisions[it]) {
-		num[it]++;
 		hlumi_itrig[it].Fill(*lumi);
 	    }
 	}
@@ -250,9 +248,7 @@ int rate(char const* input) {
     hlumi.SetName(("hlumi");
     hlumi.Write();
     for (int it=0;it<16;it++) {
-	hlumi[it].SetName(("hlumi_"+to_string(it)).c_str());
 	hlumi_itrig[it].SetName(("hlumi_itrig"+to_string(it)).c_str());
-	hlumi[it].Write();
 	hlumi_itrig[it].Write();
     }
     fout->Close();
