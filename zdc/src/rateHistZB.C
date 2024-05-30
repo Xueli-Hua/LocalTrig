4aaa4dec12acff4f107255b7ce4d50d893a1be5c
Xueli Hua <150328932+Xueli-Hua@users.noreply.github.com>
2024-05-30 11:18:27 +0800

    Update rateHistZB.C

--- zdc/src/rateHistZB.C
+++ zdc/src/rateHistZB.C
@@ -124,39 +124,30 @@ int rate(char const* input, char const* output) {
     for (auto const & name: names) trignames << name.c_str() << endl;
     trignames.close();*/
     
-    string seedzdc = "L1_SingleJet8_ZDC1n_AsymXOR"; 
+    /*string seedzdc = "L1_SingleJet8_ZDC1n_AsymXOR"; 
     string seedzb = "L1_ZeroBias_copy";
     string seedsgmo = "L1_SingleMuOpen";
     if (SeedBit.find(seedzdc.c_str()) == SeedBit.end()) return false;
     bool l1uGTdecision1;
     bool l1uGTdecision2;
     bool l1uGTdecision3;
+    Double_t zdcnum=0;
+    Double_t zbnum=0;
+    Double_t sgmonum=0;
 
     vector<string> seeds={"L1_ZeroBias_copy","L1_SingleMu5","L1_SingleMu3","L1_SingleJet8_BptxAND","L1_SingleJet16","L1_SingleJet28","L1_SingleEG5","L1_SingleEG15","L1_SingleMuOpen_SingleEG15","L1_ZDC1n_Bkp1_OR","L1_SingleJet8_ZDC1n_AsymXOR","L1_SingleJet12_ZDC1n_AsymXOR","L1_SingleJet16_ZDC1n_AsymXOR","L1_SingleJet20_ZDC1n_AsymXOR","L1_SingleJet24_ZDC1n_AsymXOR","L1_SingleJet28_ZDC1n_AsymXOR","L1_SingleJet8_ZDC1n_OR","L1_SingleJet12_ZDC1n_OR","L1_SingleJet16_ZDC1n_OR","L1_SingleJet20_ZDC1n_OR","L1_SingleJet24_ZDC1n_OR","L1_SingleJet28_ZDC1n_OR","L1_ZDC1n_AsymXOR","L1_ZDC1n_OR"};
     bool l1uGTEmudecisions[24];
-    //bool l1uGTdecisions[24];
-    Double_t num[24];
+    Double_t num[24];*/
 
+    //vector<UInt_t> runRange880 = {375245,375252,375256,375259,375300,375317,375413,375441,375448,375455,375463,375658,375665,375697,375703};
+    //vector<UInt_t> runRange1088 = {374925,374970,375007,375013,375055,375058,375064,375110,375145,375164,375195,375202};
+
+    for (auto const & name: names) {
+	if (SeedBit.find(name.c_str()) == SeedBit.end()) return false;
+    }
     Int_t Nseeds = names.size();
     bool l1uGTEmu[Nseeds];
-    //bool l1uGT[Nseeds];
     Int_t npass[Nseeds];
-    
-    // read in emulated information
-    TChain emuChain("l1UpgradeEmuTree/L1UpgradeTree");
-    FillChain(emuChain, files);
-    TTreeReader emuReader(&emuChain);
-    TTreeReaderValue<vector<float> > emuSum(emuReader, "sumZDCEt");
-    TTreeReaderValue<vector<short>> emuType(emuReader, "sumZDCType"); 
-    TTreeReaderValue<vector<float>>	emuBx(emuReader, "sumZDCBx");
-	 
-    // read in l1UpgradeTree 
-    TChain unpackerChain("l1UpgradeTree/L1UpgradeTree");
-    FillChain(unpackerChain, files);
-    TTreeReader unpackerReader(&unpackerChain);
-    TTreeReaderValue<vector<float> > unpackerSum(unpackerReader, "sumZDCEt");
-    TTreeReaderValue<vector<short>>	unpackerType(unpackerReader, "sumZDCType");
-    TTreeReaderValue<vector<float>>	unpackerBx(unpackerReader, "sumZDCBx");
 	 
     // read in l1EventTree
     TChain l1EvtChain("l1EventTree/L1EventTree");
@@ -168,20 +159,16 @@ int rate(char const* input, char const* output) {
     // create histograms
     TH1F runNbHist("runNb","runNb",900,374900,375800);
     TH1F hlumi("lumiNb","lumiNb",1000,0,1000);
-    TH1F hlumi_itrig[24];
+    /*TH1F hlumi_itrig[24];
     for (int i=0;i<24;i++) {
     	hlumi_itrig[i].SetName("lumiNb");
 	hlumi_itrig[i].SetTitle("lumiNb");
 	hlumi_itrig[i].SetBins(1000,0,1000);
-    }
+    }*/
 	 
-    Double_t zdcnum=0;
-    Double_t zbnum=0;
-    Double_t sgmonum=0;
-    //vector<UInt_t> runRange880 = {375245,375252,375256,375259,375300,375317,375413,375441,375448,375455,375463,375658,375665,375697,375703};
-    //vector<UInt_t> runRange1088 = {374925,374970,375007,375013,375055,375058,375064,375110,375145,375164,375195,375202};
     Int_t NEvts=0;
     Long64_t totalEvents = l1uGTReader.GetEntries(true);
+
     // read in information from TTrees 
     for (Long64_t i = 0; i < totalEvents; i++) {
         l1uGTReader.Next();l1uGTEmuReader.Next();unpackerReader.Next();emuReader.Next();l1EvtReader.Next();
@@ -196,7 +183,7 @@ int rate(char const* input, char const* output) {
 	if (*runNb!=375703) continue;
 	NEvts++;
 
-        if (SeedBit[seedzdc.c_str()]>=m_algoDecisionInitial_Emu.GetSize()) continue;  
+        /*if (SeedBit[seedzdc.c_str()]>=m_algoDecisionInitial_Emu.GetSize()) continue;  
         l1uGTdecision1 = m_algoDecisionInitial_Emu.At(SeedBit[seedzdc.c_str()]);
         l1uGTdecision2 = m_algoDecisionInitial_Emu.At(SeedBit[seedzb.c_str()]);
         l1uGTdecision3 = m_algoDecisionInitial_Emu.At(SeedBit[seedsgmo.c_str()]);
@@ -211,26 +198,21 @@ int rate(char const* input, char const* output) {
 		num[it]++;
 		hlumi_itrig[it].Fill(*lumi);
 	    }
-	}
+	}*/
 
 	for (unsigned int is=0;is<names.size();is++) {
 	    l1uGTEmu[is]=m_algoDecisionInitial_Emu.At(SeedBit[names[is].c_str()]);
-	    //l1uGT[is]=m_algoDecisionInitial_unpacker.At(SeedBit[names[is].c_str()]);
 	    if (l1uGTEmu[is]) npass[is]++;
 	}
 
     }
-    cout << "L1_SingleJet8_ZDC1n_AsymXOR rate: " << zdcnum << "/" << NEvts << " = " << zdcnum/NEvts << endl;
-    cout << "L1_ZeroBias_copy rate: " << zbnum << "/" << NEvts << " = " << zbnum/NEvts << endl;
-    cout << "L1_SingleMuOpen rate: " << sgmonum << "/" << NEvts << " = " << sgmonum/NEvts << endl;
-
     const std::map<uint, int> BrNb_ = {    {0, 1088},    {1, 880},    {2, 960},    {3, 394},    {4, 204},	{5,875} };
 
-    ofstream Parttrig;
+    /*ofstream Parttrig;
     Parttrig.open("results/Parttrig_"+string(output)+".txt");
     for (int i=0;i<24;i++) {
 	Parttrig << "Nb of Branches: " << BrNb_.at(1) << ", " << seeds[i].c_str() << " rate: " << num[i] << "/" << NEvts << "*11245.6*" << BrNb_.at(1) << " = " << num[i]/NEvts*11245.6*BrNb_.at(1) << endl;
-    }
+    }*/
 
     ofstream trigrates;
     trigrates.open("results/trigRates_"+string(output)+".txt");
@@ -244,10 +226,10 @@ int rate(char const* input, char const* output) {
     runNbHist.Write(); 
     hlumi.SetName("hlumi");
     hlumi.Write();
-    for (int it=0;it<24;it++) {
+    /*for (int it=0;it<24;it++) {
 	hlumi_itrig[it].SetName(("hlumi_itrig"+to_string(it)).c_str());
 	hlumi_itrig[it].Write();
-    }
+    }*/
     fout->Close();
    
     return 0;
