a237149806e04b194a933f59add6e09a3ff8dad2
Xueli Hua <150328932+Xueli-Hua@users.noreply.github.com>
2024-06-10 23:14:59 +0800

    Update UPCMu.C

--- l1rate/src/UPCMu.C
+++ l1rate/src/UPCMu.C
@@ -85,42 +85,6 @@ int Efficiency(char const* input) {
     vector<string> files;
     GetFiles(input, files);
 
-    // read in reco mu and trk information 
-    /*TChain recoMuChain("muonAnalyzer/MuonTree");
-    TChain trkChain("PbPbTracks/trackTree");
-    FillChain(recoMuChain, files);
-    FillChain(trkChain, files);
-    TTreeReader recoMuReader(&recoMuChain);
-    TTreeReader trkReader(&trkChain);
-
-    TTreeReaderValue<int> nTrk(trkReader, "nTrk");
-    TTreeReaderArray<bool> isFake(trkReader, "isFakeVtx");
-    TTreeReaderArray<bool> trkHP(trkReader, "highPurity");
-    TTreeReaderArray<float> xVtx(trkReader, "xVtx");
-    TTreeReaderArray<float> yVtx(trkReader, "yVtx");
-    TTreeReaderArray<float> zVtx(trkReader, "zVtx");
-    TTreeReaderValue<int> recomuN(recoMuReader, "nReco");
-    TTreeReaderArray<float> recomuP(recoMuReader, "recoP");
-    TTreeReaderArray<float> recomuPt(recoMuReader, "recoPt");
-    TTreeReaderArray<float> recomuEta(recoMuReader, "recoEta");
-    TTreeReaderArray<bool> recomuIsTrk(recoMuReader, "recoIsTracker");
-    TTreeReaderArray<bool> recomuIDSoft(recoMuReader, "recoIDSoft"); // mu.passed(reco::Muon::SoftCutBasedId))
-    TTreeReaderArray<bool> recomuIDHySoft(recoMuReader, "recoIDHybridSoft");
-    TTreeReaderValue<int> innermuN(recoMuReader, "nInner");
-    TTreeReaderArray<int> innermuTrkL(recoMuReader, "innerTrkLayers");
-    TTreeReaderArray<int> innermuPixL(recoMuReader, "innerPixelLayers");
-    TTreeReaderArray<float> innerDxy(recoMuReader, "innerDxy");
-    TTreeReaderArray<float> innerDz(recoMuReader, "innerDz");
-    TTreeReaderArray<bool> innerIsHPTrk(recoMuReader, "innerIsHighPurityTrack");
-    
-    // read in emulated mu information 
-    TChain l1Chain("l1object/L1UpgradeFlatTree");
-    FillChain(l1Chain, files);
-    TTreeReader l1Reader(&l1Chain);
-    TTreeReaderValue<vector<float>> l1muEt(l1Reader, "muonEt");
-    TTreeReaderValue<vector<float>> l1muEta(l1Reader, "muonEta");
-    TTreeReaderValue<vector<unsigned short>> l1muQual(l1Reader, "muonQual");*/
-
     // read in L1uGT information 
     TChain l1uGTChainForBit("l1uGTTree/L1uGTTree");
     FillChain(l1uGTChainForBit, files);
@@ -155,11 +119,10 @@ int Efficiency(char const* input) {
     for (auto const & name: names) trignames << name.c_str() << endl;
     trignames.close();
     
-    //string seed = "L1_SingleMuOpen_NotMinimumBiasHF2_AND_BptxAND"; 
-    string seedzdc = "L1_ZDC1n_Bkp1_OR"; 
+    string seedmb = "L1_Centrality_30_100_MinimumBiasHF1_AND_BptxAND"; 
     string seedtrue = "L1_AlwaysTrue";
     string seedsgmo = "L1_SingleMuOpen";
-    if (SeedBit.find(seedzdc.c_str()) == SeedBit.end()) return false;
+    if (SeedBit.find(seedmb.c_str()) == SeedBit.end()) return false;
     bool l1uGTdecision1;
     bool l1uGTdecision2;
     bool l1uGTdecision3;
@@ -170,25 +133,21 @@ int Efficiency(char const* input) {
     TTreeReader l1UpgReader(&l1UpgChain);
     TTreeReaderArray<float> sumZDCEt(l1UpgReader, "sumZDCEt");
 
-    // create histograms for efficiency plots 
-    int nbins = 25;
-    float min = 0;
-    float max = 10;
-    TH1F zVtxHist("zVtxHist", "", 200, 0, 200);
-    TH1F l1muHist("l1muHist", "", nbins, min, max);
-    TH1F recomuHist("recomuHist", "", nbins, min, max);
-
-    Double_t zdcnum=0;
-    Double_t truenum=0;
-    Double_t sgmonum=0;
+    Int_t mbnum=0;
+    Int_t truenum=0;
+    Int_t sgmonum=0;
+    Int_t mbANDsgmonum=0;
+    Int_t NEvts=0;
     Long64_t totalEvents = l1uGTReader.GetEntries(true);
     // read in information from TTrees 
     for (Long64_t i = 0; i < totalEvents; i++) {
         l1uGTReader.Next();l1UpgReader.Next();
-        //l1Reader.Next(); recoMuReader.Next(); trkReader.Next(); 
         if (i % 20000 == 0) { 
             cout << "Entry: " << i << " / " <<  totalEvents << endl; 
         }
+
+        if (*runNb!=375703) continue;
+	      NEvts++;
 
         if (SeedBit[seedzdc.c_str()]>=m_algoDecisionInitial.GetSize()) continue;  
         l1uGTdecision1 = m_algoDecisionInitial.At(SeedBit[seedzdc.c_str()]);
@@ -196,77 +155,16 @@ int Efficiency(char const* input) {
         l1uGTdecision3 = m_algoDecisionInitial.At(SeedBit[seedsgmo.c_str()]);
         cout << "iEvt: " << i << ", sumZDCEt.size: " << sumZDCEt.GetSize() << endl;
         if (l1uGTdecision1) {
-          zdcnum++;
+            mbnum++;
+            if (l1uGTdecision3) mbANDsgmonum++;
         }
         if (l1uGTdecision2) truenum++;
         if (l1uGTdecision3) sgmonum++;
-      
-        /*int NtrkHP = 0;
-        bool primaryVertex=false;
-
-        // iterate through trks and do selection 
-        for (int i = 0; i < *nTrk; ++i) {
-            //rho = TMath::Sqrt(xVtx[i]*xVtx[i]+yVtx[i]*yVtx[i]);
-            zVtxHist.Fill(TMath::Abs(zVtx[i]));
-            if (!isFake[i] && TMath::Abs(zVtx[i])<25 && !(TMath::Sqrt(xVtx[i]*xVtx[i]+yVtx[i]*yVtx[i])>2)) primaryVertex = true;
-            if (trkHP[i]) NtrkHP++;
-        }
-        if (!primaryVertex) continue;
-        if (NtrkHP!=2) continue;
-
-        // iterate through l1object muons and find max et 
-        for (int i = 0; i < *recomuN; ++i) {
-            if (recomuP[i]>2.5 && TMath::Abs(recomuEta[i]) < 2.4 && recomuIsTrk[i] && recomuIDHySoft[i]) { 
-                recomuHist.Fill(recomuPt[i]); 
-                if (l1uGTdecision) l1muHist.Fill(recomuPt[i]);
-            }
-        }
-        */
     }
-    cout << "L1_ZDC1n_Bkp1_OR rate: " << zdcnum << "/" << totalEvents << " = " << zdcnum/totalEvents << endl;
-    cout << "L1_AlwaysTrue rate: " << truenum << "/" << totalEvents << " = " << truenum/totalEvents << endl;
-    cout << "L1_SingleMuOpen rate: " << sgmonum << "/" << totalEvents << " = " << sgmonum/totalEvents << endl;
-    TGraphAsymmErrors RecoMuEff(&l1muHist, &recomuHist);
-    
-    // plot the turn ons vs reco mu pt 
-    TCanvas recoCanvas("recoCanvas", "", 0, 0, 500, 500);
-    recoCanvas.cd();
-
-    RecoMuEff.GetXaxis()->SetTitle("Reco #mu pT (GeV)");
-    RecoMuEff.GetXaxis()->CenterTitle(true);
-    RecoMuEff.GetYaxis()->SetTitle("Efficiency");
-    RecoMuEff.GetYaxis()->CenterTitle(true);
-    RecoMuEff.GetXaxis()->SetLimits(0,11);
-    RecoMuEff.SetMinimum(0);
-
-    RecoMuEff.SetMarkerColor(46);
-    RecoMuEff.SetLineColor(46);
-    RecoMuEff.SetMarkerSize(0.5);
-    RecoMuEff.SetMarkerStyle(20);
-    RecoMuEff.Draw("AP");
-
-    TLegend recoLegend(0.13, 0.12 ,0.88, 0.2);
-    recoLegend.SetBorderSize(0);
-    recoLegend.SetFillStyle(0);
-    recoLegend.SetTextSize(0.03);
-    recoLegend.SetHeader(seedzdc.c_str());
-    //recoLegend.AddEntry(&RecoMuEff, "#DeltaR Matched", "lep");
-    recoLegend.Draw();
-
-    TLine recoline(0,1,11,1);
-    recoline.SetLineColor(46);
-    recoline.Draw();
-
-    recoCanvas.SaveAs("results/non.pdf");
-
-    // save histograms to file so I can look at them 
-    TFile* fout = new TFile("results/non.root", "recreate");
-
-    zVtxHist.Write();
-    l1muHist.Write();
-    recomuHist.Write();
-
-    fout->Close();
+    cout << "L1_Centrality_30_100_MinimumBiasHF1_AND_BptxAND rate: " << setw(20) << mbnum << "*11245.6*880" << "/" << NEvts << " = "  << setw(20) << mbnum*11245.6*880/NEvts << endl;
+    cout << "L1_AlwaysTrue rate: " << setw(20) << truenum << "*11245.6*880" << "/" << NEvts << " = "  << setw(20) << truenum*11245.6*880/NEvts << endl;
+    cout << "L1_SingleMuOpen rate: " << setw(20) << sgmonum << "*11245.6*880" << "/" << NEvts << " = "  << setw(20) << sgmonum*11245.6*880/NEvts << endl;
+    cout << "L1_Centrality_30_100_MinimumBiasHF1_AND_BptxAND + L1_SingleMuOpen rate: " << setw(20) << mbANDsgmonum << "*11245.6*880" << "/" << NEvts << " = "  << setw(20) << mbANDsgmonum*11245.6*880/NEvts << endl;
    
     return 0;
 }
